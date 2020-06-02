f56192ddc23d308f61ffe2328ad2053522015d8e
francisdcunha07 <francis.dcunha06@gmail.com>
2020-06-02 23:38:12 +0530

    added a test case for tracking event

--- Js/rivraddon.js
+++ Js/rivraddon.js
@@ -1,13 +1,27 @@
-window.rivraddon = window.rivraddon || {}; 
-window.rivraddon.analytics =  window.rivraddon.analytics || {};
+window = window || {};
+window.rivraddon = window.rivraddon || {};
+window.rivraddon.analytics = window.rivraddon.analytics || {};
 window.rivraddon.analytics.enableAnalytics = function () {
     console.log('SIMPLAEX CODE CHALLENGE LOG rivraddon analytics.enableAnalytics');
 }
 
 window.rivraddon.analytics.trackPbjsEvent = function (event) {
-    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-    var theUrl = "https://tracker.simplaex-code-challenge.com";
-    xmlhttp.open("POST", theUrl);
-    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xmlhttp.send(JSON.stringify({ "eventType": event.eventType }));
+    return new Promise(function (resolve, reject) {
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject("Some Error Occurred");
+                }
+            }
+        };
+        xhr.open('POST', "https://tracker.simplaex-code-challenge.com")
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.send(JSON.stringify({ "eventType": event.eventType }));
+    });
 }
+
+
+
--- Js/rivraddon.test.js
+++ Js/rivraddon.test.js
@@ -1,16 +1,39 @@
-let testFile =  require("./rivraddon.js");;
+let testFile = require("./rivraddon.js");;
 
 global.console = {
-    log: jest.fn()
-  }
- 
-  describe('Tests rivraddon', () => {
-    test('should console a message', () => {
-      const output ="SIMPLAEX CODE CHALLENGE LOG rivraddon analytics.enableAnalytics";
-      window.rivraddon.analytics.enableAnalytics();
-      expect(global.console.log).toHaveBeenCalledWith(
-        output
-      )
-    })
+  log: jest.fn()
+}
 
+describe('Tests rivraddon', () => {
+  test('should console a message', () => {
+    const output = "SIMPLAEX CODE CHALLENGE LOG rivraddon analytics.enableAnalytics";
+    window.rivraddon.analytics.enableAnalytics();
+    expect(global.console.log).toHaveBeenCalledWith(
+      output
+    );
   })
+});
+
+const mockXHR = {
+  open: jest.fn(),
+  send: jest.fn(),
+  readyState: 4,
+  onreadystatechange: jest.fn(),
+  setRequestHeader: jest.fn()
+};
+window.XMLHttpRequest = jest.fn(() => mockXHR);
+
+describe("Testing the track event using Xhr", function () {
+  test("Should post event to tracker", function (done) {
+    const reqPromise = window.rivraddon.analytics.trackPbjsEvent({ "eventType": "Purchase" });
+    mockXHR.onreadystatechange();
+    reqPromise.then(res => {
+      done();
+    }).catch(e => {
+      expect(mockXHR.open).toBeCalledWith('POST', 'https://tracker.simplaex-code-challenge.com');
+      expect(mockXHR.setRequestHeader).toBeCalledWith('Content-Type', 'application/json;charset=UTF-8');
+      expect(e).toBe("Some Error Occurred");
+      done();
+    });
+  });
+});
